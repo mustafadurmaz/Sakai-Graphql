596294d3c9b762d9bfb21ecbad22399eb3b81375
mustafadurmaz <mdmz_001@hotmail.com>
2023-09-04 23:30:39 +0300

    i18n layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,10 +1,32 @@
-
+import { NextIntlClientProvider } from "next-intl";
+import { notFound } from "next/navigation";
 import Layout from "../../layout/layout";
+
+export function generateStaticParams() {
+  return [{ locale: "en" }, { locale: "de" }];
+}
 
 interface AppLayoutProps {
   children: React.ReactNode;
 }
 
-export default function AppLayout({ children }: AppLayoutProps) {
-  return <Layout>{children}</Layout>;
+export default async function AppLayout(
+  { children }: AppLayoutProps,
+  { params: { locale } }: any
+) {
+  let messages;
+  try {
+    messages = (await import(`../../messages/${locale}.json`)).default;
+  } catch (error) {
+    notFound();
+  }
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
 }
